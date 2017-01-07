84414f9159663ae2b61ed1734ef37b7a3f126fc0
novlike <novlike@gmail.com>
2017-01-07 17:40:19 +0100

    1st draft pour mon site CV

---
+++ hschoi site CV/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		<title>Hwa-Seon CHOI - Web developer</title>
+	</head>
+	<body>
+		<header>
+			<h1>Hwa-Seon Choi</h1>
+			<p>Web Developer</p>
+			<nav>
+				
+			</nav>
+		</header>
---
+++ hschoi site CV/index.php
@@ -0,0 +1,12 @@
+<?php 
+	include('header.php');
+?>
+		<main>
+			<section>
+				<p>Hello, World! I'm a young developer based in Paris, coming from Seoul. I wish to be a UX design-oriented front-end developer with aims to make your every website visit agreeable.</p>
+				<p>Since graduating with a B.A. in french literature and also M.A in cinema, I have committed to promote immerging and/or "auteur" artists. </p>
+			</section>
+		</main>
+<?php 
+	include('footer.php');
+?>
