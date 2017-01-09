5b0e395217d10bbabbe76c095760c7dd787856d4
novlike <novlike@gmail.com>
2017-01-09 17:28:09 +0100

    un peu d'ajax et fichier complet chasse aux trésors de long hai

--- hschoiCV/header.php
+++ hschoiCV/header.php
@@ -6,13 +6,20 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="css/style.css">
+		<link rel="icon" href="img/favicon.ico">
 		<title>Hwa-Seon CHOI - Web developer</title>
 	</head>
 	<body>
 		<header>
 			<h1>Hwa-Seon Choi</h1>
 			<p>Web Developer</p>
+			<h2>I build websites in the office, take photographs outside and enjoy eating inside.</h2>
 			<nav>
-				
+				<ul>
+					<li>About</li>
+					<li>Portfolio</li>
+					<li>Find me more</li>
+					<li>Hire me!</li>
+				</ul>
 			</nav>
 		</header>
--- hschoiCV/index.php
+++ hschoiCV/index.php
@@ -3,8 +3,7 @@
 ?>
 		<main>
 			<section>
-				<p>Hello, World! I'm a young developer based in Paris, coming from Seoul. I wish to be a UX design-oriented front-end developer with aims to make your every website visit agreeable.</p>
-				<p>Since graduating with a B.A. in french literature and also M.A in cinema, I have committed to promote immerging and/or "auteur" artists. </p>
+				
 			</section>
 		</main>
 <?php 
